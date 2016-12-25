0ada53cfd13296f1f104e8f69ce0a338c44f9b07
Nathan Wallace <techietotoro@gmail.com>
2016-12-24 23:05:33 -1000

    Adds support for loading page data from an external JSON file.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cssStandards = require('spike-css-standards')
 const jsStandards = require('babel-preset-latest')
 const pageId = require('spike-page-id')
 const postcssEasyImport = require('postcss-easy-import')
+const Records = require('spike-records')
+const locals = {}
 
 module.exports = {
   devtool: 'source-map',
@@ -16,7 +18,7 @@ module.exports = {
   reshape: (ctx) => {
     return htmlStandards({
       webpack: ctx,
-      locals: { pageId: pageId(ctx), foo: 'bar' }
+      locals: Object.assign({ pageId: pageId(ctx) }, locals)
     })
   },
   postcss: (ctx) => {
@@ -30,6 +32,10 @@ module.exports = {
       environmentPaths: { root: __dirname },
       recordsPath: path.join(__dirname, '_cache/records.json'),
       cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
+    }),
+    new Records({
+      addDataTo: locals,
+      data: { file: 'data/data.json' }
     })
   ]
 }
